1825f4b2629b707b89782c16f6faf29cec119ff6
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2020-07-13 21:17:53 +0600

    Added dependencies munus,uuid,ext-json

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
